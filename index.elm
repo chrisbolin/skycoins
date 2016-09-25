004c19cb856915b96eaee6448922a9cf73c1328e
Chris Bolin <bolin.chris@gmail.com>
2016-09-25 12:44:27 -0400

    using animation frame and very rough physics

--- index.elm
+++ index.elm
@@ -1,8 +1,8 @@
-import Html exposing (Html, button, div, text, h1)
+import Html exposing (Html, button, div, text, h1, h3)
 import Html.App as App
 import Html.Events exposing (onClick)
 import Html.Attributes exposing ( style )
-import Time exposing (second)
+import AnimationFrame
 import Debug exposing (log)
 import Keyboard exposing (KeyCode)
 
@@ -22,7 +22,13 @@ type alias Model =
   {
     mainEngine: Bool,
     rightThruster: Bool,
-    leftThruster: Bool
+    leftThruster: Bool,
+    x: Float,
+    y: Float,
+    theta: Float,
+    dx: Float,
+    dy: Float,
+    dTheta: Float
   }
 
 -- Update
@@ -35,8 +41,20 @@ type Msg
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Tick timestamp ->
-      (model, Cmd.none)
+    Tick intervalLengthMs ->
+      let
+        intervalLength = intervalLengthMs / 100
+        dy1 = model.dy - 1 * intervalLength
+          + if model.mainEngine then 10 * intervalLength else 0
+        y1 = model.y + dy1 * intervalLength
+      in
+        (
+          {model
+            | dy = dy1
+            , y = y1
+          }
+          , Cmd.none
+        )
     KeyDown code ->
       case code of
         37 ->
@@ -65,7 +83,7 @@ subscriptions model =
   Sub.batch
     [ Keyboard.downs KeyDown
       , Keyboard.ups KeyUp
-      , Time.every second Tick
+      , AnimationFrame.diffs Tick
     ]
 
 
@@ -81,10 +99,15 @@ view model =
           h1 [] [ text (toString model.mainEngine)]
           , h1 [] [ text (toString model.leftThruster)]
           , h1 [] [ text (toString model.rightThruster)]
+          , h3 [] [ model.y |> round |> toString |> text ]
+          , h3 [] [ model.dy |> round |> toString |> text ]
       ]
 
 -- Init
 
 init : (Model, Cmd Msg)
 init =
-  (Model False False False, Cmd.none)
+  (
+    Model False False False 0 0 0 0 0 0,
+    Cmd.none
+  )
